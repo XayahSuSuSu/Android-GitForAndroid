5ccbc568301b9752d8b41f864d051259e648ea51
Xayah <zds1249475336@gmail.com>
2022-03-03 21:16:03 +0800

    feat: Initialize shell

--- app/src/main/java/com/xayah/gitforandroid/App.kt
+++ app/src/main/java/com/xayah/gitforandroid/App.kt
@@ -1,9 +1,30 @@
 package com.xayah.gitforandroid
 
 import android.app.Application
+import android.content.Context
+import com.topjohnwu.superuser.Shell
+import com.xayah.gitforandroid.util.Path
+
 
 class App : Application() {
     companion object {
-        init {}
+        init {
+            Shell.enableVerboseLogging = BuildConfig.DEBUG
+            Shell.setDefaultBuilder(
+                Shell.Builder.create()
+                    .setFlags(Shell.FLAG_REDIRECT_STDERR or Shell.FLAG_NON_ROOT_SHELL)
+                    .setTimeout(10)
+                    .setInitializers(EnvInitializer::class.java)
+            )
+        }
+    }
+
+    class EnvInitializer : Shell.Initializer() {
+        override fun onInit(context: Context, shell: Shell): Boolean {
+            shell.newJob()
+                .add("export HOME=${Path.getExternalFilesDir(context)}")
+                .exec()
+            return true
+        }
     }
 }
