7126e9b94f587cc5b7cf2ea2650782dbb23803db
Xayah <zds1249475336@gmail.com>
2022-03-07 17:47:08 +0800

    feat: Add git version card in Home

--- app/src/main/java/com/xayah/gitforandroid/fragment/home/HomeViewModel.kt
+++ app/src/main/java/com/xayah/gitforandroid/fragment/home/HomeViewModel.kt
@@ -1,5 +1,8 @@
 package com.xayah.gitforandroid.fragment.home
 
 import androidx.lifecycle.ViewModel
+import com.topjohnwu.superuser.ShellUtils
 
-class HomeViewModel : ViewModel() {}
+class HomeViewModel : ViewModel() {
+    val gitVersion = ShellUtils.fastCmd("git --version").replace("git version ", "")
+}
